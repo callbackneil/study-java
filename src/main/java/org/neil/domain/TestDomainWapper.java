e636c01904a4bb5eed0d1946d2197644819628f1
1640921692 <1640921692@qq.com>
2018-05-18 15:18:29 +0800

    test

--- src/main/java/org/neil/domain/TestDomainWapper.java
+++ src/main/java/org/neil/domain/TestDomainWapper.java
@@ -6,15 +6,24 @@ package org.neil.domain;
  */
 public class TestDomainWapper {
 
-    private Long id;
+    private Long       id;
     private TestDomain testDomain;
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public TestDomain getTestDomain() {
+        return testDomain;
+    }
+
     public void setTestDomain(TestDomain testDomain) {
 
         this.testDomain = testDomain;
+
     }
 }

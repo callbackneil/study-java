61c31437287de09011180642a94c2620e53e8ffd
zhangzhen <1640921692@qq.com>
2019-07-03 17:55:35 +0800

    test

--- src/main/java/org/neil/controller/TestController.java
+++ src/main/java/org/neil/controller/TestController.java
@@ -1,6 +1,7 @@
 package org.neil.controller;
 
 import org.neil.domain.TestDomainWapper;
+import org.neil.manager.TestManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,20 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @Autowired
-    private TestDomainWapper testDomainWapper;
-
-    public void setTestDomainWapper(TestDomainWapper testDomainWapper) {
-        this.testDomainWapper = testDomainWapper;
-    }
+    private TestManager testManager;
 
     @RequestMapping(value = "test1", method = RequestMethod.GET)
     public Object argumentResolverController(TestDomainWapper testDomainWapper) {
         return testDomainWapper;
     }
 
-    @RequestMapping(value = "test2", method = RequestMethod.GET)
-    public Object argumentResolverController2() {
-        return this.testDomainWapper;
+    @RequestMapping(value = "test3", method = RequestMethod.GET)
+    public Object asyncTest() {
+        System.out.println("执行异步方法调用前");
+        testManager.asnyTest();
+        System.out.println("执行异步方法调用后");
+        return "success";
     }
 
 
--- src/main/java/org/neil/util/TestArguementResolver.java
+++ src/main/java/org/neil/util/TestArguementResolver.java
@@ -1,13 +1,13 @@
 package org.neil.util;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.neil.domain.TestDomainWapper;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author neil
@@ -34,27 +34,5 @@ public class TestArguementResolver implements HandlerMethodArgumentResolver {
 
         return null;
     }
-
-//    public static void main(String[] args) {
-//        Integer i1 = Integer.valueOf(100);
-//        int i2 = new Integer(100);
-//        Integer i3  = Integer.valueOf(100);
-//        System.out.println(i1 == i2);
-//        System.out.println(i1 == i3);
-//        System.out.println(i2 == i3);
-//    }
-
-    public static void main(String[] args) {
-        int i = 0;
-        int[] arr = {10,9,8,7,6,5,4,3,2,1,0};
-        int index = 0;
-        for (; i < 11; i++) {
-            int nextIndex = (index+1) % arr.length;
-            index = nextIndex;
-            System.out.println(arr[index]);
-        }
-    }
-
-
 
 }

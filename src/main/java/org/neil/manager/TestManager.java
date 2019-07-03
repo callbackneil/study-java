5b99e6ebb4665f6195211ddf016086779f5295e9
zhangzhen <1640921692@qq.com>
2019-07-03 17:58:01 +0800

    test

---
+++ src/main/java/org/neil/manager/TestManager.java
@@ -0,0 +1,10 @@
+package org.neil.manager;
+
+/**
+ * @author neil
+ * @date 2019-07-03
+ */
+public interface TestManager {
+
+    void asnyTest();
+}
---
+++ src/main/java/org/neil/util/JustTest.java
@@ -0,0 +1,13 @@
+package org.neil.util;
+
+/**
+ * @author neil
+ * @date 2019-07-02
+ */
+public class JustTest {
+    public static void main(String[] args) {
+        String s= "{\"newProducts\":[{\"originSkuCode\":\"570560102\",\"num\":1,\"targetSkuCode\":\"569670102\"}],\"tidPayTime\":{\"newValue\":\"2019-07-02 09:50:36\",\"oldValue\":\"2019-07-02 09:50:36.0\"},\"detailBOS\":{\"oldValue\":[{\"createTime\":\"2019-07-02 17:37:05.0\",\"id\":441,\"isDelete\":0,\"num\":1,\"numId\":\"557360316897\",\"oid\":\"513386243584448328\",\"originColorId\":44429,\"originMetricsId\":85443,\"originSkuCode\":\"570560102\",\"productId\":29268,\"registerId\":100238,\"saleId\":56465,\"targetColorId\":43846,\"targetMetricsId\":83789,\"targetSkuCode\":\"564650102\",\"tid\":\"513386243582448328\",\"updateTime\":\"2019-07-02 17:49:06.33\",\"version\":4}]},\"createTime\":{\"oldValue\":\"2019-07-02 17:32:59.0\"},\"isDelete\":{\"oldValue\":0},\"tidCreateTime\":{\"newValue\":\"2019-07-02 09:50:27\",\"oldValue\":\"2019-07-02 09:50:27.0\"},\"updateTime\":{\"oldValue\":\"2019-07-02 17:49:06.327\"},\"oldProducts\":[{\"originSkuCode\":\"570560102\",\"num\":1,\"targetSkuCode\":\"564650102\"}]}";
+        System.out.println(s.length());
+
+    }
+}

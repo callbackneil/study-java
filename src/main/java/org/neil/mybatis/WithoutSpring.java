28da7b35376357262d15dc30ce59e9b87637c88d
zhangzhen <1640921692@qq.com>
2019-07-05 18:27:15 +0800

    mybatis

---
+++ src/main/java/org/neil/mybatis/WithoutSpring.java
@@ -0,0 +1,33 @@
+package org.neil.mybatis;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.neil.dao.PCDao;
+
+/**
+ * @author neil
+ * @date 2019-07-05
+ */
+public class WithoutSpring {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mapper/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try{
+            PCDao pcDao = sqlSession.getMapper(PCDao.class);
+            String maxId = pcDao.getMAXId();
+            System.out.println(maxId);
+        }finally {
+            sqlSession.close();
+        }
+
+
+    }
+}

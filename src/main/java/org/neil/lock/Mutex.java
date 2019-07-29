8fdede3edc1a1b83de4e31ad6900b34686e6fa2c
callbackneil <neil_zz@163.com>
2019-07-29 23:40:01 +0800

    mybatis-generator

---
+++ src/main/java/org/neil/lock/Mutex.java
@@ -0,0 +1,89 @@
+package org.neil.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Created by zhangzhen on 2019/7/27.
+ */
+public class Mutex implements Lock {
+    // 队列同步器的具体实现
+    private final Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1,unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+
+
+
+
+    /**
+     * 自定义同步器实现AQS   重写独占式的获取与释放锁的方法和查询当前线程是否占用锁的方法
+     */
+    private static class Sync extends AbstractQueuedSynchronizer{
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            // 尝试去占用锁 占用成功 将当前线程设为持有锁的线程
+            if(compareAndSetState(0,1)){
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            // 如果处于无所状态 抛出异常 否则直接释放锁 （将持有锁的线程设为null）
+            if(getState() == 0){
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(Thread.currentThread());
+            setState(0);
+            return true;
+        }
+
+        /**
+         * 是否处于占用状态 state==1时 表示占用
+         * @return
+         */
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        Condition newCondition(){
+            return new ConditionObject();
+        }
+    }
+
+}
